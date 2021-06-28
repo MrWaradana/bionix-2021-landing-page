a0455f1fa47477b0d096cb889c66c3811fcd50b8
MrWaradana <muhammadridhowaradana@gmail.com>
2021-06-29 00:59:35 +0700

    landing page 2.0

--- js/custom.js
+++ js/custom.js
@@ -7,3 +7,16 @@ $(document).ready(function(){
         }
     });
 });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
